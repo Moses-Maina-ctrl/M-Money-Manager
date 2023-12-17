1e0db2d557b0720059a4d490e7d779ced0b09cd8
Moses-Maina-ctrl <mosesmaina1245@gmail.com>
2023-12-17 16:55:46 +0300

    Fix the findFile function in dataExtractor

---
+++ expensePackage/dataExtractor.py
@@ -0,0 +1,14 @@
+from lxml import etree
+
+def findfile(file):
+    try:
+        doc = etree.parse(file)
+        root = doc.getroot()
+        for element in root.iter():
+            print(root.tag)
+    except Exception as e:
+        print(f"Error reading the XML file: {e}")
+
+xml_path = "/mnt/c/Users/Moses/Documents/projects/rubbish/sms-20231217000653.xml"
+findfile(xml_path)
+
